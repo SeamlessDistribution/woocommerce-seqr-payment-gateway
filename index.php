19ad6cb7f64366a1b36369ba90ca7f3a1ca4a094
Erik Hedenström <erik@hedenstroem.com>
2014-05-20 22:30:07 +0200

    Initial implementation

---
+++ index.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Plugin Name: WooCommerce SEQR Payment Gateway
+Plugin URI: http://developer.seqr.com/woocommerce
+Description: SEQR Payment gateway for woocommerce
+Version: 0.1.0
+Author: Erik Hedenström
+Author URI: http://se.linkedin.com/in/ehedenst
+*/
+
+add_action('plugins_loaded', 'woocommerce_seqr_init', 0);
+
+function woocommerce_seqr_init()
+{
+    if (!class_exists('WC_Payment_Gateway')) return;
+
+    class WC_SEQR_Payment_Gateway extends WC_Payment_Gateway
+    {
+
+        public function __construct()
+        {
+
+            $this ->id = 'SEQR';
+            $this ->medthod_title = __('SEQR', 'woocommerce');;
+            // $this->method_description = '';
+            // $this->icon = 'http://...'; 
+            $this->has_fields = false;
+
+            $this->init_form_fields();
+            $this ->init_settings();
+
+            $this ->title = $this ->settings['title'];
+            $this ->description = $this ->settings['description'];
+            $this ->wsdl_url = $this ->settings['wsdl_url'];
+            $this ->terminal_id = $this ->settings['terminal_id'];
+            $this ->terminal_password = $this ->settings['terminal_password'];
+
+            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+            add_action('woocommerce_receipt_seqr', array(&$this, 'receipt_page'));
+
+        }
+
+        function init_form_fields()
+        {
+            $this->form_fields = array(
+                'enabled' => array(
+                    'title' => __('Enable/Disable', 'seqr'),
+                    'type' => 'checkbox',
+                    'label' => __('Enable SEQR Checkout', 'seqr'),
+                    'default' => 'no'
+                ),
+                'title' => array(
+                    'title' => __('Title', 'seqr'),
+                    'type' => 'text',
+                    'description' => __('This controls the title which the user sees during checkout.', 'seqr'),
+                    'default' => __('SEQR Mobile Payment', 'seqr'),
+                    'desc_tip' => true,
+                ),
+                'description' => array(
+                    'title' => __('Description', 'seqr'),
+                    'type' => 'textarea',
+                    'default' => __('Pay securely with your mobile phone.', 'seqr'),
+                    'description' => __('This controls the description which the user sees during checkout.', 'seqr'),
+                ),
+                'wsdl_url' => array(
+                    'title' => __('WSDL URL', 'seqr'),
+                    'type' => 'text',
+                    'description' => __('WSDL URL.', 'seqr'),
+                    'default' => '',
+                    'desc_tip' => true,
+                ),
+                'terminal_id' => array(
+                    'title' => __('Terminal ID', 'seqr'),
+                    'type' => 'text',
+                    'description' => __('Terminal ID.', 'seqr'),
+                    'default' => '',
+                    'desc_tip' => true,
+                ),
+                'terminal_password' => array(
+                    'title' => __('Terminal Password', 'seqr'),
+                    'type' => 'text',
+                    'description' => __('Terminal Password.', 'seqr'),
+                    'default' => '',
+                    'desc_tip' => true,
+                )
+            );
+        }
+
+        public function admin_options()
+        {
+            echo '<h3>' . __('SEQR Payment Gateway', 'seqr') . '</h3>';
+            echo '<p>' . __('SEQR is the premiere solution for mobile payments') . '</p>';
+            echo '<table class="form-table">';
+            $this ->generate_settings_html();
+            echo '</table>';
+        }
+
+        /**
+         *  There are no payment fields for seqr, but we want to show the description if set.
+         **/
+        function payment_fields()
+        {
+            if ($this ->description) echo wpautop(wptexturize($this ->description));
+        }
+
+        /**
+         * Receipt Page
+         **/
+        function receipt_page($order)
+        {
+            echo '<p>' . __('Thank you for your order, please click the button below to pay with SEQR.', 'seqr') . '</p>';
+            echo $this ->generate_seqr_form($order);
+        }
+
+        /**
+         * Generate SEQR form
+         **/
+        public function generate_seqr_form($order_id)
+        {
+            return "QR code HERE!";
+        }
+
+        /**
+         * Process the payment and return the result
+         **/
+        function process_payment($order_id)
+        {
+            global $woocommerce;
+            $order = new WC_Order($order_id);
+            // Mark as on-hold (we're awaiting the cheque)
+            $order->update_status('on-hold', __('Awaiting SEQR payment', 'seqr'));
+            // Reduce stock levels
+            $order->reduce_order_stock();
+            // Remove cart
+            $woocommerce->cart->empty_cart();
+            // Return thankyou redirect
+            return array(
+                'result' => 'success',
+                'redirect' => $this->get_return_url($order)
+            );
+        }
+    }
+
+
+    /**
+     * Add the Gateway to WooCommerce
+     **/
+    function woocommerce_add_seqr_gateway($methods)
+    {
+        $methods[] = 'WC_SEQR';
+        return $methods;
+    }
+
+    add_filter('woocommerce_payment_gateways', 'woocommerce_add_seqr_gateway');
+}
+
+?>
